88b761a992517b678cf5201548695ed1b917846d
rmn_tmx <reverb6569@gmail.com>
2022-01-19 16:40:28 +0100

    autogit

--- log-api-stats.sh
+++ log-api-stats.sh
@@ -4,12 +4,28 @@ DIR="/data/data/com.termux/files/home/api_logs"
 FILE="$(date +"%m-%d-%y-%T").log"
 
 cd $DIR
-touch $FILE
 
 # log api calls:
+echo "battery:" >> $FILE
+echo "---" >> $FILE
 termux-battery-status >> $FILE
+
+echo "infrared-frequencies:" >> $FILE
+echo "---" >> $FILE
 termux-infrared-frequencies >> $FILE
+
+echo "location:" >> $FILE
+echo "---" >> $FILE
 termux-location >> $FILE
-termux-sensor >> $FILE
+
+# echo "sensors:" >> $FILE
+# echo "---" >> $FILE
+# termux-sensor -a >> $FILE
+
+echo "telephone-cell-info:" >> $FILE
+echo "---" >> $FILE
 termux-telephony-cellinfo >> $FILE
+
+echo "wifi-connection-info:" >> $FILE
+echo "---" >> $FILE
 termux-wifi-connectioninfo >> $FILE
